90f58682d6da8edb0a02cb8f252cd940dcf167a5
Lassi Kortela <lassi@lassi.io>
2022-07-06 23:18:50 +0300

    Find top sections of file

--- sensible.py
+++ sensible.py
@@ -7,10 +7,36 @@ import sys
 import pose
 
 
+def is_head(head, x):
+    return isinstance(x, list) and isinstance(x[0], pose.Symbol) and x[0].name == head
+
+
+def dehead(head, x):
+    if not is_head(head, x):
+        raise Exception("What?")
+    return x[1:]
+
+
+def assoc(head, xs):
+    for x in xs:
+        if is_head(head, x):
+            return x[1:]
+    raise Exception("What?")
+
+
 def main():
-    forms = pose.PoseReader(sys.stdin).read_all()
-    for form in forms:
-        print(repr(form))
+    tops = pose.PoseReader(sys.stdin).read_all()
+    if len(tops) != 1:
+        raise Exception("What?")
+    top = dehead("ansible", tops[0])
+    options = assoc("options", top)
+    groups = assoc("groups", top)
+    playbooks = assoc("playbooks", top)
+    roles = assoc("roles", top)
+    print(repr(options))
+    print(repr(groups))
+    print(repr(playbooks))
+    print(repr(roles))
 
 
 if __name__ == "__main__":
